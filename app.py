fb5a7e10e93ec4e33d56b19d3f6e322a4a5059b1
Jakub Maciasz <57491280+jakubmac1@users.noreply.github.com>
2025-05-24 16:37:30 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import requests
+import os
 from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
 from flask import Flask, jsonify
@@ -129,4 +130,5 @@ def get_mecze():
     return jsonify({"mecze": wszystkie_mecze})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
